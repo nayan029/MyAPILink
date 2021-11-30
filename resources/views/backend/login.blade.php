3e1599a5181306c3aa59b4e3f0f5423f21d795dc
Mukesh Rathod <mukeshrathod.vhits@gmail.com>
2021-11-30 14:22:37 +0530

    Login Design Related Changes

--- resources/views/backend/login.blade.php
+++ resources/views/backend/login.blade.php
@@ -18,7 +18,23 @@
   <script src="//cdnjs.cloudflare.com/ajax/libs/twitter-bootstrap/3.3.1/js/bootstrap.min.js"></script>
   <link rel="stylesheet" type="text/css" href="https://cdnjs.cloudflare.com/ajax/libs/toastr.js/latest/toastr.min.css">
 
-
+<style>
+  .card-primary.card-outline {
+    border-top: 3px solid #74bfd4;
+}
+.btn-primary {
+    color: #fff;
+    background-color: #19c3f3;
+    border-color: #19c3f3;
+    box-shadow: none;
+}
+.btn-warning {
+    color: #1f2d3d;
+    background-color: #e9a523;
+    border-color: #e9a523;
+    box-shadow: none;
+}
+</style>
 </head>
 
 <body class="hold-transition login-page">
@@ -26,7 +42,7 @@
     <!-- /.login-logo -->
     <div class="card card-outline card-primary">
       <div class="card-header text-center">
-        <a href="javascript:void(0);" class="h1"><b>Admin</b>LTE</a>
+        <a href="javascript:void(0);" class="h1"><img src="{{asset('/frontend/logo_dark.png')}}" style="max-width: 50%;height: auto;"></a>
       </div>
       <div class="card-body">
         <p class="login-box-msg">Sign in to start your session</p>

81c2b04c03f2c6ad1583bcff80d76cfd5bcd8c4f
yogeshrathodvh <yogeshrathod@virtualheight.com>
2021-12-03 18:08:04 +0530

    manager changes

--- app/Repositories/ManagerRepository.php
+++ app/Repositories/ManagerRepository.php
@@ -8,7 +8,7 @@ use Illuminate\Http\Request;
 use Illuminate\Support\Facades\Auth;
 use Illuminate\Support\Facades\Hash;
 use Illuminate\Support\Facades\Mail;
-
+use App\Interfaces\ManagerRepositoryInterface;
 
 class ManagerRepository implements ManagerRepositoryInterface
 {

b31463e37b1affe57cc14e05bb2e1259696f991f
nileshthakkar0816 <nileshthakkar.vhits@gmail.com>
2021-12-03 13:13:20 +0530

    Contact Related Changes

--- resources/views/backend/skills/edit.blade.php
+++ resources/views/backend/skills/edit.blade.php
@@ -88,8 +88,8 @@
                                 @foreach($skill_position as $key=>$sk)
                                 <tr row_id="{{$key+1}}">
                                     <td>{{$key+1}}</td>
-                                    <td class="xyz">{{$sk->position}}<input type="hidden" class="position" name="position[]" value="{{$sk->position}}"></td>
-                                    <td>{{$sk->title}}<input type="hidden" class="title" name="title[]" value="{{$sk->title}}"></td>
+                                    <td class="xyz">{{$sk->position}}<input type="hidden" class="position" name="position[]" value="{{$sk}}"></td>
+                                    <td>{{$sk->title}}<input type="hidden" class="title" name="title[]" value=""></td>
                                     <td>{{$sk->desc}}<input type="hidden" class="desc" name="descs[]" value="{{$sk->desc}}"></td>
                                     <td><a href='javascript:void(0);' class="edit_row" row_id="{{$key+1}}"><i class='fas fa-edit'></i></a>
                                         <a href='javascript:void(0);' class="btn btn-save" data-id="{{$sk->id}}" row_id="{{$key+1}}"><i class="fas fa-save text-success"></i></i></a>
@@ -242,7 +242,7 @@
             var title = $('.title').val(t);
             var description = $('.desc').val(d);
             var id = $(this).attr("data-id");
-            updateSkillPostionData(position, title, description, id);
+            updateSkillPostionData(position, title, description, id);   
 
         });
         $(document).on('click', ".removeRow", function() {

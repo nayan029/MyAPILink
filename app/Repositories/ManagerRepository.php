e8f5ac32d7e2a9ec35ce3b2759d256faabe85597
Mukesh Rathod <mukeshrathod.vhits@gmail.com>
2021-12-06 10:14:09 +0530

    Model namespace add

--- app/Repositories/ManagerRepository.php
+++ app/Repositories/ManagerRepository.php
@@ -9,6 +9,7 @@ use Illuminate\Support\Facades\Auth;
 use Illuminate\Support\Facades\Hash;
 use Illuminate\Support\Facades\Mail;
 use App\Interfaces\ManagerRepositoryInterface;
+use App\Models\EmailTemplate;
 
 class ManagerRepository implements ManagerRepositoryInterface
 {

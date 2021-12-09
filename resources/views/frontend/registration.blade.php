2f2b9a53e4f17b9480200a72606ee0a3bebd500c
Mukesh Rathod <mukeshrathod.vhits@gmail.com>
2021-12-09 13:02:48 +0530

    Register page and widget related changes

--- resources/views/frontend/registration.blade.php
+++ resources/views/frontend/registration.blade.php
@@ -136,7 +136,7 @@
                             <button id="loginbtn" type="submit"  class="btn btn-blue w-100">Connexion</button>
                         </div>
                         <div class=" meconnecter">
-                            <p class="proxima-nove">Pas encore membre?<a href="#" class=""> Inscrivez-vous</a></p>
+                            <p class="proxima-nove">Pas encore membre?<a href="javascript:void(0);" class="openRegister"> Inscrivez-vous</a></p>
                         </div>
                     </div>
                 </form>
@@ -182,7 +182,7 @@
                             </div>
                             <div class="col-md-12">
                                 <div class="form-group">
-                                    <input type="text" name="phone" placeholder="Téléphone" class="form-control login-input inputicon2" id="phone">
+                                    <input type="text" name="phone" placeholder="Téléphone" class="form-control login-input inputicon2" onkeypress="return isNumber(event)" id="phone" maxlength="10">
                                     <span class="text-danger error" id="phone-error"></span>
                                 </div>
                             </div>
@@ -225,7 +225,7 @@
 
                             <div class="col-md-12 text-center res-dec mt-3">
                                 <div class=" meconnecter Connectez-vous">
-                                    <p class="">Déjà membre?<a href="#" class=""> Connectez-vous</a></p>
+                                    <p class="">Déjà membre?<a href="javascript:void(0);" class="openLogin"> Connectez-vous</a></p>
                                 </div>
                             </div>
                         </div>
@@ -278,6 +278,14 @@
 <script src="{{asset('frontend/js/custom.js')}} "></script>
 <script src="https://cdnjs.cloudflare.com/ajax/libs/toastr.js/latest/js/toastr.min.js"></script>
 <script>
+     function isNumber(evt) {
+        evt = (evt) ? evt : window.event;
+        var charCode = (evt.which) ? evt.which : evt.keyCode;
+        if (charCode > 31 && (charCode < 48 || charCode > 57)) {
+            return false;
+        }
+        return true;
+    }
     $(function() {
 
         @if(Session::has('success'))
@@ -298,6 +306,14 @@
     });
 </script>
 <script>
+    $(document).on('click','.openLogin',function(){
+        $("#header-Modallogin1").modal('hide');
+        $("#Modallogin2").modal('show');
+    });
+    $(document).on('click','.openRegister',function(){
+        $("#header-Modallogin1").modal('show');
+        $("#Modallogin2").modal('hide');
+    });
     $(document).on('click', '#registerUser', function() {
 
         var token = $("meta[name='csrf-token']").attr("content");

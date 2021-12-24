466483975dfa838cc933d93e76f73dbf6a3294a4
DharaBarmeda <dhara.vhits@gmail.com>
2021-12-24 13:12:01 +0530

    gallery changes done

--- app/Interfaces/GalleryRepositoryInterface.php
+++ app/Interfaces/GalleryRepositoryInterface.php
@@ -7,4 +7,5 @@ use Illuminate\Http\Request;
 interface GalleryRepositoryInterface
 {
     public function getGallerydata(Request $request);
+    public function getUpdatedStatus(Request $request);
 }
